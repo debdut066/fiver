50dd9bd91f8c2a063748738ccd6c8c21c43c4703
debdut066 <nilkarpas@gmail.com>
2023-04-03 12:03:15 +0530

    updated the gig page

--- client/src/pages/gigs/Gigs.jsx
+++ client/src/pages/gigs/Gigs.jsx
@@ -5,14 +5,54 @@ import { gigs } from "../../data"
 
 
 const Gigs = () => {
+  const [sort, setSort] = React.useState("sales")
+  const [open, setOpen] = React.useState(false);
+  const minRef = React.useRef(null);
+  const maxRef = React.useRef(null);
+
+  const reSort = (type) => {
+    setSort(type)
+    setOpen(false)
+  }
+
+  const apply = () => {
+    console.log(minRef.current.value)
+    console.log(maxRef.current.value)
+  }
+
   return (
     <div className='gigs'>
       <div className='container'>
-        {/* <span className='breadcrumbs'>
+        <span className='breadcrumbs'>
           fiverr / Graphics & Design
         </span>
         <h1>AI Artists</h1>
-        <p>Explore the boundaries of art and technology with fiverr's AI artists</p> */}
+        <p>Explore the boundaries of art and technology with fiverrs AI artists</p>
+        <div className="menu">
+          <div className='left'>
+            <span>Budget</span>
+            <input ref={minRef} type="number" placeholder='min' />
+            <input ref={maxRef} type="number" placeholder='max' />
+            <button onClick={apply}>Apply</button>
+          </div>
+          <div className='right'>
+            <span className='sortBy'>Sort by</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
+              </div>
+            )}
+          </div>
+        </div>
         <div className="cards">
           {gigs.map((gig) => (
             <GigCard key={gig.id} item={gig} />
