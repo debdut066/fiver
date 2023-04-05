51d80c5a4479db038581aa59b2cccd323de9d715
debdut066 <nilkarpas@gmail.com>
2023-04-05 12:42:34 +0530

    created the server

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import db from "./database.js";
+import cors from "cors";
+import userRoute from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+dotenv.config();
+
+//Importing user routes
+app.use("api/user", userRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
+app.listen(process.env.PORT, () => {
+  db();
+  console.log("backend server running");
+});
