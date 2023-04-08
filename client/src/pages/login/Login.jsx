72e6bd895786ed9f92bbf43d37cb21050edc19ab
debdut066 <nilkarpas@gmail.com>
2023-04-08 12:24:22 +0530

    created the login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 import "./Login.scss"
 
 const Login = () => {
+  const [error, setError] = React.useState(null)
+
   return (
-    <div>Login</div>
+    <div className='login'>
+      <form >
+        <h1>Sign in</h1>
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+          placeholder='johndoe'
+          onChange={() => { }}
+        />
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          onChange={() => { }}
+        />
+      </form>
+      <button type='submit'>Login</button>
+      {error && <span>{error}</span>}
+    </div>
   )
 }
 
