fab226ae7ad15e90b7edf9608be45324b133ed32
debdut066 <nilkarpas@gmail.com>
2023-04-13 12:02:04 +0530

    added react query config

--- client/src/Layout/DefaultLayout/DefaultLayout.jsx
+++ client/src/Layout/DefaultLayout/DefaultLayout.jsx
@@ -3,13 +3,21 @@ import Navbar from "../Navbar";
 import Footer from "../Footer";
 import { Outlet } from "react-router-dom";
 import "./DefaultLayout.scss";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const DefaultLayout = () => {
   return (
     <div className="wrapper">
-      <Navbar />
-      <Outlet />
-      <Footer />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </QueryClientProvider>
     </div>
   );
 };
